f2c104209230ec1d49fcd36ecffef3cf207c2aa4
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-05-31 00:10:51 +0000

    a function that compares two values and prints a colorful message depending on the outcome

--- assertEqual.js
+++ assertEqual.js
@@ -1,5 +1,5 @@
 const assertEqual = function(actual, expected) {
-  // Comparing two values, prints a message depending if its equal or not
+  // Comparing two values, prints a message depending if its equal or notg
   if (actual === expected) {
     console.log(`👏🏽👏🏽👏🏽  Assertion Passed: ${actual} === ${expected}`);
   } else {
