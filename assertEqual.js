6989b2e2d2ecc119a2fd7a08679a9f381fe69ac2
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-05-31 00:09:19 +0000

    a function that compares two values and prints a colorful message depending on the outcome

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  // Comparing two values, prints a message depending if its equal or not
+  if (actual === expected) {
+    console.log(`👏🏽👏🏽👏🏽  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌  Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1,1);
+assertEqual('one', 'one');
+assertEqual(1,6);
