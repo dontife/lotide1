e69e6c9a860a96b007ac3739dbf3bc91e8b231bc
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-06-01 22:15:10 +0000

    The function takes in a sentence (as a string) and then return a count of each of the letters in that sentence

---
+++ countLetters.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+  // Comparing two values, prints a message depending if its equal or notg
+  if (actual === expected) {
+    console.log(`👏🏽👏🏽👏🏽  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌  Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+// create a function that prints out how many times a letter occurs in a setence
+const countLetters = function(sentence)  {
+  // remove the space in the string
+  let str = sentence.replace(/\s+/g, '');
+  //declare a varaibale result  of type object to hold the value of the result
+  let result = {};
+  // iterate through the setence
+  for (const letter of str) {
+    // increment the value of the keys that occur more than once in the setence
+    if (result[letter]) {
+      result[letter] += 1;
+    } else {
+      // assign a value = 1 for all the letters present in the setence
+      result[letter] = 1;
+    }
+  }
+  return result;
+
+};
+
+
+//Test Cases
+let test1 = countLetters("lighthouse in the house");
+console.log(countLetters("lighthouse in the house"));
+assertEqual(test1['g'], 1);
