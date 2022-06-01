b734f1a1a88ae4a7a0b7a506bf0fe3ae1afaec44
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-06-01 21:48:47 +0000

    This function takes in a collection of items and return counts for a specific subset of those items

---
+++ countOnly.js
@@ -0,0 +1,53 @@
+const assertEqual = function(actual, expected) {
+  // Comparing two values, prints a message depending if its equal or notg
+  if (actual === expected) {
+    console.log(`👏🏽👏🏽👏🏽  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌  Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  // iterating over the array
+  for (const item of allItems) {
+    // if the value of the key in the object is true then perform some calculations
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+
+
+
+
+
+
+// Test Cases
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
