c39470f0b7ba4973c191fbeafe2b0c89eeee344b
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-06-02 20:56:45 +0000

    helps find key

---
+++ findKey.js
@@ -0,0 +1,45 @@
+const assertEqual = function(actual, expected) {
+  // Comparing two values, prints a message depending if its equal or notg
+  if (actual === expected) {
+    console.log(`👏🏽👏🏽👏🏽  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌  Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+const findKey = function (obj, callback){
+  let temp;
+  Object.entries(obj).forEach(([key, value]) => {
+    console.log(callback(value));
+     if(callback(value) === true && temp === undefined){
+       console.log('here');
+       temp = key;
+     }
+  })
+  return temp; 
+}
+
+
+
+//Test cases 
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)) // => "noma"
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) , 'noma')
+
