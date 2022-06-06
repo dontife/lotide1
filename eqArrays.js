ba7ef74cbcbf76faf3766215c21b794076650cdd
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-06-06 22:53:33 +0000

    refactored the code by making using of the export command

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  // Comparing two values, prints a message depending if its equal or notg
-  if (actual === expected) {
-    console.log(`👏🏽👏🏽👏🏽  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌  Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
 
 const eqArrays = function(actual, expected) {
   for (let i = 0; i < actual.length; i++) {
@@ -19,6 +10,4 @@ const eqArrays = function(actual, expected) {
   return true;
 };
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 3, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
