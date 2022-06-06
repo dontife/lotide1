757fac5c28fbcfb7348f987c9800c5cc6ad713e6
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-06-06 19:17:31 +0000

    refactored the code by making using of the require/import command

--- eqArrays.js
+++ eqArrays.js
@@ -8,16 +8,16 @@ const assertEqual = function(actual, expected) {
 
 };
 
-const eqArrays = function (actual, expected){
-  for(let i = 0; i < actual.length; i++) {
-    // if the content in both arrays do not match, return false 
-    if(actual[i] !== expected[i]) {
-      return false 
+const eqArrays = function(actual, expected) {
+  for (let i = 0; i < actual.length; i++) {
+    // if the content in both arrays do not match, return false
+    if (actual[i] !== expected[i]) {
+      return false;
     }
   }
   // else return true;
   return true;
-}
+};
 
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
