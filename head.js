9df06f857d048bc268786b36df9849151e73bd51
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-06-06 19:17:52 +0000

    refactored the code by making using of the require/import command

--- head.js
+++ head.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual, expected) {
-  // Comparing two values, prints a message depending if its equal or notg
-  if (actual === expected) {
-    console.log(`👏🏽👏🏽👏🏽  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌  Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 
 const head = function(someArray) {
@@ -14,7 +6,5 @@ const head = function(someArray) {
   return someArray[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Labs');
-assertEqual(head([6]), 6);
-assertEqual(head([]), undefined);
+
+module.exports = head;
