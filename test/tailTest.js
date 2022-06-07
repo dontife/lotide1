340c2fbb2bb2241cc156118ca1e80d299cf51411
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-06-07 00:47:33 +0000

    refactor the code by using moch and chai library for testcases

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,15 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
+const assert = require('chai').assert;
+const tail = require('../tail'); 
 
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+describe('#tail', () => {
+  it("check that the original array has not been modified", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result.length, 2);
+  });
+  it("returns ['lighthouse', 'labs'] for an array of  ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ['Lighthouse', 'Labs']);
+  });
+});
+
+
